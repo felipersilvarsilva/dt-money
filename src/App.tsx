26d682c8c944ecdcdce56df3e0f912c540549a5d
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-04 23:56:48 +0000

    feat: Add preconnect links for Google Fonts, import ThemeProvider from styled-components, import defaultTheme from ./styles/theme/default, import GlobalStyle from ./styles/globals, add ThemeProvider wrapper with defaultTheme and GlobalStyle, create styled.d.ts file, create GlobalStyle component and add defaultTheme object

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/globals";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
