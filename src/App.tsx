d324334f01d26df01d4fe1901d490c9f3bff112b
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-05 00:23:46 +0000

    feat: Fixed import, added TransactionsProvider, added TransactionsContext and TransactionsProvider and added axios instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/globals";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div className="App">
+      <TransactionsProvider>
         <h1>Hello</h1>
-      </div>
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
