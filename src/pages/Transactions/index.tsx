704150c8d920e1a5712c8527f080c449fa213637
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-10 17:50:48 +0000

    feat: Add Summary component and style Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,64 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TansactionsContainer,
+  TansactionsTable,
+} from "./styles";
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
+      <TansactionsContainer>
+        <TansactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">conta de água </td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 12.000</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">lanches </td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 12.000</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TansactionsTable>
+      </TansactionsContainer>
     </div>
   );
 }
