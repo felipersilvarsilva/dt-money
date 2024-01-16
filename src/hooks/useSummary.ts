16c6164605b5ecdd819f5e0b0c078561eceac25d
felipe <106001741+felipersilvarsilva@users.noreply.github.com>
2024-01-16 13:23:33 +0000

    feat: Add useMemo hook to improve performance and format code with proper indentation

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
@@ -5,23 +6,26 @@ export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions;
   });
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === "income") {
+          acc.income += transaction.price;
+          acc.total += transaction.price;
+        } else {
+          acc.outcome += transaction.price;
+          acc.total -= transaction.price;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+  }, [transactions]);
 
   return summary;
 }
